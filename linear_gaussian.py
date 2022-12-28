d001a46500dcb13b87633af9f56a1083e75d7fc5
frbm <frbm@pm.me>
2022-12-28 10:24:19 +0100

    first commit

---
+++ linear_gaussian.py
@@ -0,0 +1,161 @@
+from math import pi
+from vsmc import *
+from torch.optim import Adam
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+
+torch.manual_seed(0)
+
+
+class LinearGaussianStateSpaceSMC(VariationalSMC):
+    def __init__(self, dx, dy, alpha, r, obs, n, t, scale):
+        super().__init__(dx, dy, alpha, r, obs, n, t, scale)
+
+    def generate_data(self, t=5):
+        mu_0, s_0, a, q, c, rr = self.model_params
+        dx = mu_0.size(0)
+        dy = rr.size(0)
+
+        x = torch.zeros((t, dx))
+        y = torch.zeros((t, dy))
+
+        for s in range(t):
+            if s > 0:
+                mean = torch.matmul(a, x[s - 1, :])
+                dist = MultivariateNormal(loc=mean, covariance_matrix=q)
+                x[s, :] = dist.rsample()
+            else:
+                dist = MultivariateNormal(loc=mu_0, covariance_matrix=s_0)
+                x[s, :] = dist.rsample()
+
+            mean = torch.matmul(c, x[s, :])
+            dist = MultivariateNormal(loc=mean, covariance_matrix=rr)
+            y[s, :] = dist.rsample()
+
+        return x, y
+
+    def log_marginal_likelihood(self, t, y):
+        mu_0, s_0, a, q, c, rr = self.model_params
+        dx = mu_0.size(0)
+        dy = rr.size(0)
+
+        log_likelihood = 0.
+        x_fil = torch.zeros(dx)
+        p_fil = torch.zeros((dx, dx))
+        x_pred = mu_0
+        p_pred = s_0
+
+        for s in range(t):
+            if s > 0:
+                x_pred = torch.matmul(a, x_fil)
+                p_pred = torch.matmul(a, torch.matmul(p_fil, a.T)) + q
+
+            # update
+            y_s = y[s, :] - torch.matmul(c, x_pred)
+            b = torch.matmul(c, torch.matmul(p_pred, c.T)) + rr
+            k = torch.linalg.solve(b, torch.matmul(c, p_pred)).T
+            x_fil = x_pred + torch.matmul(k, y_s)
+            p_fil = p_pred - torch.matmul(k, torch.matmul(c, p_pred))
+
+            sgn, log_det = torch.linalg.slogdet(b)
+            log_likelihood -= 0.5 * (torch.sum(y_s * torch.linalg.solve(b, y_s)) + log_det + dy * log(2 * pi))
+
+        return log_likelihood
+
+    @staticmethod
+    def log_normal(x, mu, sigma):
+        dim = sigma.size(0)
+        sgn, log_det = torch.linalg.slogdet(sigma)
+        log_norm = - 0.5 * (dim * log(2 * pi) + log_det)
+        p_rec = torch.linalg.inv(sigma)
+
+        test2 = x - mu
+        test1 = torch.matmul(p_rec, (x - mu).T)
+
+        log_norm = log_norm - 0.5 * torch.sum(torch.mul((x - mu), torch.matmul(p_rec, (x - mu).T).T), dim=1)
+        return log_norm
+
+    def helper(self, t, x_p):
+        print(t)
+        mu_0, s_0, a, q, c, rr = self.model_params
+        mu_t, lint, log_s2t = self.prop_params[(3*t):(3*t+3)]
+        s2t = torch.exp(log_s2t)
+
+        if t > 0:
+            mu = mu_t + torch.matmul(a, x_p.T).T * lint
+        else:
+            mu = mu_t + lint * mu_0
+        return mu, s2t
+
+    def log_prop(self, t, x_c, x_p):
+        mu, s2t = self.helper(t, x_p)
+        return self.log_normal(x_c, mu, torch.diag(s2t))
+
+    def log_target(self, t, x_c, x_p, y):
+        mu_0, s_0, a, q, c, rr = self.model_params
+
+        if t > 0:
+            log_f = self.log_normal(x_c, torch.matmul(a, x_p.T).T, q)
+        else:
+            log_f = self.log_normal(x_c, mu_0, s_0)
+        log_g = self.log_normal(torch.matmul(c, x_c.T).T, y[t], rr)
+        return log_f + log_g
+
+    def log_weights(self, t, x_c, x_p, y):
+        target = self.log_target(t, x_c, x_p, y)
+        prop = self.log_prop(t, x_c, x_p)
+        return target - prop
+
+    def sim_prop(self, t, x_p):
+        mu, s2t = self.helper(t, x_p)
+        return mu + torch.randn(*x_p.size()) * torch.sqrt(s2t)
+
+    def objective(self, y, adaptive_resampling=False):
+        return - self.forward(y, adaptive_resampling=adaptive_resampling)
+
+
+if __name__ == '__main__':
+    # Model hyper-parametersf
+    t = 10
+    dx = 5
+    dy = 3
+    alpha = 0.42
+    r = .1
+    obs = 'sparse'
+
+    # Training parameters
+    scale = 0.5
+    epochs = 1000
+    lr = 0.001
+    printing_freq = 10
+
+    n = 4
+
+    smc_model = LinearGaussianStateSpaceSMC(dx, dy, alpha, r, obs, n, t, scale)
+
+    optimizer = Adam(smc_model.prop_params, lr=lr)
+
+    print("Generating data...")
+    x_true, y_true = smc_model.generate_data(t)
+
+    lml_true = smc_model.log_marginal_likelihood(t, y_true)
+    print(f'True log-marginal likelihood: {lml_true.item()}.')
+    print('')
+
+    # training loop
+    for epoch in range(1, epochs+1):
+        optimizer.zero_grad()
+        loss = smc_model.objective(y_true)
+        loss.backward()
+        optimizer.step()
+
+        if epoch % printing_freq == 0:
+            print(f'Epoch {epoch} of {epochs}.')
+            print(f'Current ELBO: {-loss.item()}.')
+            print('')
+
+    print('True y:')
+    print(y_true)
+    print('')
+    print('Simulated y:')
+    print(smc_model.sim_q(y_true))
---
+++ vsmc.py
@@ -0,0 +1,164 @@
+from math import log
+import torch
+
+torch.manual_seed(0)
+
+
+class VariationalSMC:
+    def __init__(self, dx, dy, alpha, r, obs, n, t, scale):
+        self.dx = dx
+        self.dy = dy
+        self.alpha = alpha
+        self.r = r
+        self.obs = obs
+        self.n = n
+
+        self.model_params = self.init_model_params(dx, dy, alpha, r, obs)
+        self.prop_params = self.init_prop_params(t, dx, scale)
+
+    @staticmethod
+    def init_model_params(dx, dy, alpha, r, obs):
+        mu_0 = torch.zeros(dx)
+        s_0 = torch.eye(dx)
+
+        a = torch.zeros((dx, dx))
+        for i in range(dx):
+            for j in range(dx):
+                a[i, j] = alpha ** (1 + abs(i - j))
+
+        q = torch.eye(dx)
+        c = torch.zeros((dy, dx))
+        if obs == 'sparse':
+            c[:dy, :dy] = torch.eye(dy)
+        else:
+            c = torch.randn((dy, dx))
+        rr = r * torch.eye(dy)
+        return mu_0, s_0, a, q, c, rr
+
+    @staticmethod
+    def init_prop_params(t, dx, scale=0.5):
+        out = []
+        for s in range(t):
+            bias = scale * torch.randn(dx)
+            times = 1 + scale * torch.randn(dx)
+            log_var = scale * torch.randn(dx)
+            out += [bias, times, log_var]
+        return out
+
+    def generate_data(self, *args):
+        raise NotImplementedError
+
+    def log_marginal_likelihood(self, *args):
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def resampling(self, w):
+        n = w.size(0)
+        bins = torch.cumsum(w, dim=-1)
+        ind = torch.arange(n)
+        u = (ind + torch.rand(n))/n
+        return torch.bucketize(u, bins)
+
+    def forward(self, y, adaptive_resampling=False, verbose=True):
+        """VSMC Lower Bound"""
+        # constants
+        t = y.size(0)
+        dx = self.dx
+        n = self.n
+
+        # initialisation
+        x = torch.zeros((n, dx))
+        log_w = torch.zeros(n)
+        w = torch.exp(log_w)
+        w /= w.sum()
+        log_z = 0.
+        ess = 1/torch.sum(torch.square(w))/n
+
+        for s in range(t):
+            # resampling
+            if adaptive_resampling:
+                if ess < 0.5:
+                    ancestors = self.resampling(w)
+                    xp = x[ancestors]
+                    log_z += torch.max(log_w) + torch.log(torch.sum(w)) - log(n)
+                    log_w = torch.zeros(n)
+                else:
+                    xp = x.clone()
+            else:
+                if t > 0:
+                    ancestors = self.resampling(w)
+                    xp = x[ancestors]
+                else:
+                    xp = x.clone()
+
+            # propagation
+            x = self.sim_prop(s, xp)
+
+            # weighting
+            if adaptive_resampling:
+                log_w += self.log_weights(s, x, xp, y)
+            else:
+                log_w = self.log_weights(s, x, xp, y)
+            max_log_w = torch.max(log_w)
+            w = torch.exp(log_w - max_log_w)
+
+            if adaptive_resampling:
+                if s == t - 1:
+                    log_z += max_log_w + torch.log(torch.sum(w)) - torch.log(w)
+            else:
+                log_z += max_log_w + torch.log(torch.sum(w)) - log(n)
+
+            w /= w.sum()
+            ess = 1/torch.sum(torch.square(w))/n
+
+            if verbose:
+                print(f'ESS: {ess}.')
+
+            return log_z
+
+    def log_weights(self, *args):
+        raise NotImplementedError
+
+    def sim_prop(self, *args):
+        raise NotImplementedError
+
+    def sim_q(self, y, verbose=True):
+        # constants
+        t = y.size(0)
+        dx = self.dx
+        n = self.n
+
+        # initialisation
+        x = torch.zeros((n, t, dx))
+        w = torch.zeros((n, t))
+        ess = torch.zeros(t)
+
+        for s in range(t):
+            # resampling
+            if t > 0:
+                ancestors = self.resampling(w[:, s-1])
+                x[:, :s, :] = x[ancestors, :t, :]
+
+            # propagation
+            x[:, t, :] = self.sim_prop(t, x[:, s-1, :])
+
+            # weighting
+            log_w = self.log_weights(t, x[:, t, :], x[:, s-1, :], y)
+            max_log_w = torch.max(log_w)
+            w[:, t] = torch.exp(log_w - max_log_w)
+            w[:, t] /= w[:, t].sum()
+            ess[t] = 1/torch.sum(torch.square(w[:, t]))
+
+        # sample from the empirical approximation
+        bins = torch.cumsum(w[:, -1], dim=-1)
+        u = torch.rand()
+        print(u)
+        print(bins)
+        b = torch.bucketize(u, bins)
+        print(b)
+
+        if verbose:
+            print(f'Mean ESS: {ess.mean()}')
+            print(f'Lowest ESS: {torch.min(ess)}')
+
+        return x[b, :, :]
